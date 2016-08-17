97d0ebdc1803a23906a724e4c3b2e1e899e120ec
Keenan <allgoodkeenan@hotmail.com>
2016-08-17 14:34:51 -0500

    Add files via upload

--- bin/logged_top.php
+++ bin/logged_top.php
@@ -3,16 +3,15 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>FakeBook</title>
-		<link rel="stylesheet" href="./bin/style.css">
+		<link rel="stylesheet" href="../bin/style.css">
 	</head>
 	<body>
 		<header><h2>FakeBook</h2></header>
 		<nav>
 			<ul>
-				<li><a href="./?action=profile">My Profile</a></li>
-				<li><a href="./?action=update">Update Profile</a></li>
-				<li><a href="./?action=feed">News Feed</a></li>
-				<li><a href="./?action=logout">Log out</a></li>
+				<li><a href="./update_view.php">Update Profile</a></li>
+				<li><a href="./newsfeed.php">News Feed</a></li>
+				<li><input type="submit" value="Log Out" id="btnlogout"/></li>
 			<ul>
 			
 		</nav>
