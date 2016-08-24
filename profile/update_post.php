75dffead4f519589f4dd87a22974e52d121b8f09
Austin Stutzman <stuzguy@gmail.com>
2016-08-24 14:19:04 -0500

    Add files via upload

--- profile/update_post.php
+++ profile/update_post.php
@@ -3,9 +3,11 @@ function confirm_update()
 {
 	$first_name = filter_input(INPUT_POST, 'firstname');
 	$last_name = filter_input(INPUT_POST, 'lastname');
+	$user_name = filter_input(INPUT_POST, 'username');
 	$password = filter_input(INPUT_POST, 'password');
 	$confirm_password = filter_input(INPUT_POST, 'confirmpassword');
 	$email_address = filter_input(INPUT_POST, 'emailaddress', FILTER_VALIDATE_EMAIL);
+	$profilepic = "user_blank.png";
 	
 	if($email_address === false || $email_address === '')
 	{
@@ -27,7 +29,12 @@ function confirm_update()
 		exit();
 	}
 	
-	update_user($_SESSION['userdata']['alias'], $password, $first_name, $last_name, $email_address, $_SESSION['userdata']['profilepic']);
+	$_SESSION['userdata']['fname'] = $first_name;
+	$_SESSION['userdata']['lname'] = $last_name;
+	$_SESSION['userdata']['email'] = $email_address;
+	$_SESSION['userdata']['profilepic'] = $profilepic;
+	
+	update_user($user_name, $password, $first_name, $last_name, $email_address, $profilepic);
 	
 	include('update_confirmation.php');
 }
