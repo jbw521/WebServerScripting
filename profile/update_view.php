d578a7c995de5a4b6a6a4f06e6a3474e0bd068d1
Austin Stutzman <stuzguy@gmail.com>
2016-08-24 14:21:05 -0500

    Add files via upload

--- profile/update_view.php
+++ profile/update_view.php
@@ -6,14 +6,15 @@
                     <p class="error"><?php echo $error_message ?></p><br>
                 <?php } ?>
 				
-				<label>UserName: <?php echo $_SESSION['userdata']['alias']; ?></label></br>
-				
                 <label>First Name:</label>
                 <input type='text' name='firstname' value="<?php echo $_SESSION['userdata']['fname']; ?>"><br>
                 
                 <label>Last Name:</label>
                 <input type='text' name='lastname' value="<?php echo $_SESSION['userdata']['lname']; ?>"><br>
-
+				
+				<label>UserName:</label>
+                <input type='text' name='username' value="<?php echo $_SESSION['userdata']['alias']; ?>"><br>
+                
                 <label>Email Address:</label>
                 <input type='email' name='emailaddress' value="<?php echo $_SESSION['userdata']['email']; ?>"><br>
 				
@@ -21,7 +22,9 @@
                 <input type='password' name='password'><br> 
 
 				<label>Confirm Password:</label>
-                <input type='password' name='confirmpassword'><br> 				
+                <input type='password' name='confirmpassword'><br>
+				<label>Profile Picture:</label>
+				<input type="file" name="img" accept="image/*"></br>
             </div>
 			<input type="hidden" name="action" value="update_profile" />
             <div id='buttons'>
@@ -29,4 +32,5 @@
             </div>
             </form>
 <?php include('../bin/master_bottom.php');
+
 ?>
